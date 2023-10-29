c3098d1023209990f7028108e3c34617aaefdaf4
mferoc <matheusferreiradeocosta@gmail.com>
2023-10-28 22:39:10 -0300

    refactor: removendo imports nao utilizados

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,8 +2,6 @@ import * as React from "react";
 import {
   createBrowserRouter,
   RouterProvider,
-  Route,
-  Link,
 } from "react-router-dom";
 import Login from "./screens/Login";
 import Tasks from "./screens/Tasks";
